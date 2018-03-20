8b08a4bb12652780bc458976fa586497145cd532
Fergus Lees <leesferg@wg401-g-04-06.mddn.vuw.ac.nz>
2018-03-21 12:39:07 +1300

    Created Unity project, a basic scene and a script for movement

---
+++ UnityProj_Abomination/Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Movement and jump speeds
+    public float MoveSpeed = 10.0f;
+
+    // Get Camera and Player Object
+    public Transform PlayerView;
+    public Transform Player;
+    private CharacterController Controller;
+
+
+    void Start()
+    {
+        // Hides cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Finds the CharacterController component in object with this script
+		Controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Get players position
+        Vector3 playerposition = Player.transform.position;
+
+        // Makes cursor lock when clicking the play window
+        if (Cursor.lockState != CursorLockMode.Locked)
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // PLAYER MOVEMENT \\
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * MoveSpeed;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * MoveSpeed;
+		transform.Translate(x-z, 0, z+x);
+
+		if (Input.GetButtonDown ("Evade"))
+			transform.Translate(5,0,0);
+    }
+}
